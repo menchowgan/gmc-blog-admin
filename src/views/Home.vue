96ed5b74b16afa41b331f1f4616a71441fe65d42
mengchao_gan <1657943220@qq.com>
2022-06-25 19:32:57 +0800

    gmc blog admin init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="admin-container flex column">
     hello vite + vue3
   </div>
 </template>
@@ -8,4 +8,16 @@
 </script>
 
 <style scoped lang="scss">
+@import "../style/theme.scss";
+@import "../style/flex-style.scss";
+
+.admin-container {
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-image: url("@/assets/images/background.webp");
+  background-size:cover;
+  background-attachment: fixed;
+}
 </style>
