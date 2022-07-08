ec7ba0475812e91cecec6514a5021a88b19b1efc
mengchao_gan <1657943220@qq.com>
2022-07-08 09:49:23 +0800

    改写pinia store为setup函数

--- src/store/audio.ts
+++ src/store/audio.ts
@@ -1,7 +1,28 @@
 import { defineStore } from 'pinia'
+import { ref, computed } from "vue"
 
-export const useAudioStore = defineStore("audio", {
-  state: () => ({
-    curidx: -1
+export const useAudioStore = defineStore("audio", () => {
+  const curidx = ref<number>(-1)
+  const curCount = ref<number>(0)
+
+  const add = () => {
+    curCount.value++
+  }
+
+  const currentCount = computed(() => {
+    return curCount.value
   })
+
+  const $reset = () => {
+    curidx.value = -1
+    curCount.value = 0
+  }
+
+  return {
+    curidx,
+    curCount,
+    add,
+    currentCount,
+    $reset
+  }
 })
