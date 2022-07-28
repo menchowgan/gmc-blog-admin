d1cab9a41375dabe65976ce046a1e8aefd266745
mengchao_gan <1657943220@qq.com>
2022-07-28 22:39:22 +0800

    modify loading timer

--- src/plugins/Loading/index.ts
+++ src/plugins/Loading/index.ts
@@ -7,8 +7,19 @@ document.body.appendChild(div)
 
 const oldvnode = createVNode(Loading, { loading: false })
 render(oldvnode, div)
-export default (loading: boolean) => {
-  const newvnode = createVNode(Loading, { loading })
+
+let timer: any = null
+
+export default (loading: boolean, delay: number = 200) => {
+  let newvnode = null
+  if (!timer && delay) {
+    timer = setTimeout(() => {
+      newvnode = createVNode(Loading, { loading })
+      clearTimeout(timer)
+    }, delay)
+  } else {
+    newvnode = createVNode(Loading, { loading })
+  }
   render(newvnode, div)
   div.style.display = "block"
   div.addEventListener("click", () => {
